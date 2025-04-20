95f93390094c006e143a8b680fe036618401352b
Alexandre Oliveira <alexandre_osvaldo@hotmail.com>
2025-04-20 15:09:15 -0300

    created pages in the project

---
+++ src/pages/Orders/Form/index.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function FormOrders() {
+	return <div>FormOrders</div>;
+}
---
+++ src/pages/Orders/List/index.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function ListOrders() {
+	return <div>ListOrders</div>;
+}
