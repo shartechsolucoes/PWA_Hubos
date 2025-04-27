8783af8ab2a51d5ef26254990d064f62b554a3a4
Edson <xpro23@gmail.com>
2025-04-26 22:13:35 -0300

    Frank

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,24 +1,32 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
-import {MdOutlineAccountCircle} from "react-icons/md";
-
-import "./style.css";
-import {IoMdAdd} from "react-icons/io";
+import {IoIosLogOut, IoMdAdd} from "react-icons/io";
 import {GoHome} from "react-icons/go";
+import {CiCircleList, CiUser} from "react-icons/ci";
+import "./style.css";
 
 export default function Navbar() {
 	return (
 		<>
-
 			<div className="container m-0 ">
 				<div className="navbar d-block">
 					<div className="row ">
-						<div className='col-4 item text-center'><a href="../dashboard"><GoHome /></a></div>
-						<div className='col-4 item text-center'>
+						<div className='w-25 item text-center'>
+							<a href="../dashboard"><GoHome /></a>
+						</div>
+						<div className='col-2 item text-center'>
+							<a href="../orders"><CiCircleList /></a>
+						</div>
+						<div className='col-2 item text-center'>
 						<div className='add'>
-							<a href="../orders"><IoMdAdd /></a>
+							<a href="../orders/form"><IoMdAdd /></a>
 						</div>
 						</div>
-						<div className='col-4 item text-center'><a href="../user/info"><MdOutlineAccountCircle /></a></div>
+						<div className='col-2 item text-center'>
+							<a href="../user/info"><CiUser /></a>
+						</div>
+						<div className='col-3 item text-center'>
+							<a href="../"><IoIosLogOut /> </a>
+						</div>
 					</div>
 				</div>
 			</div>
